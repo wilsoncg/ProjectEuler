ac315be6ac78c790b9cdac8c9b21ec0d7de7d347
Craig Wilson <craig@wilsoncg.net>
2014-02-19 22:06:59 +0000

    initial commit

---
+++ EulerFibonacci.UnitTests/FibonacciSumTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using NUnit.Framework;
+
+namespace EulerFibonacci.UnitTests
+{
+    [TestFixture]
+    public class FibonacciSumTests
+    {
+        [Test]
+        public void FirstFibonacciNumberisZero()
+        {
+            var fib = FibonnaciSum.fibToN(1);
+
+            Assert.AreEqual(0, fib);
+        }
+
+        [Test]
+        public void SecondFibonacciNumberisOne()
+        {
+            var fib = FibonnaciSum.fibToN(2);
+
+            Assert.AreEqual(1, fib);
+        }
+
+        [Test]
+        public void ThirdFibonacciNumberIsOne()
+        {
+            var fib = FibonnaciSum.fibToN(3);
+
+            Assert.AreEqual(1, fib);
+        }
+
+        [Test]
+        public void FourthFibonacciNumberIsTwo()
+        {
+            var fib = FibonnaciSum.fibToN(4);
+
+            Assert.AreEqual(2, fib);
+        }
+
+        [Test]
+        public void FifthFibonacciNumberIsThree()
+        {
+            var fib = FibonnaciSum.fibToN(5);
+
+            Assert.AreEqual(3, fib);
+        }
+
+        [Test]
+        public void SixthFibonacciNumberIsFive()
+        {
+            var fib = FibonnaciSum.fibToN(6);
+
+            Assert.AreEqual(5, fib);
+        }
+
+        [Test]
+        public void FibonacciListForFirstSixNumbersIsCorrect()
+        {
+            var fibList = FibonnaciSum.fibList(6).ToList();
+
+            Assert.AreEqual(new BigInteger(0), fibList[0]);
+            Assert.AreEqual(new BigInteger(1), fibList[1]);
+            Assert.AreEqual(new BigInteger(1), fibList[2]);
+            Assert.AreEqual(new BigInteger(2), fibList[3]);
+            Assert.AreEqual(new BigInteger(3), fibList[4]);
+            Assert.AreEqual(new BigInteger(5), fibList[5]);
+        }
+
+        [Test]
+        public void SumOfFirstFibonnaciNumberIsZero()
+        {
+            var sum = FibonnaciSum.sumFibToN(1);
+
+            Assert.AreEqual(new BigInteger(0), sum);
+        }
+
+        [Test]
+        public void SumOfFirstTwoFibonnaciNumbersIsOne()
+        {
+            var sum = FibonnaciSum.sumFibToN(2);
+
+            Assert.AreEqual(new BigInteger(1), sum);
+        }
+
+        [Test]
+        public void SumOfFirstThreeFibonacciNumbersIsTwo()
+        {
+            var sum = FibonnaciSum.sumFibToN(3);
+
+            Assert.AreEqual(new BigInteger(2), sum);
+        }
+
+        [Test]
+        public void SumOfFirstFourFibonacciNumbersIsFour()
+        {
+            var sum = FibonnaciSum.sumFibToN(4);
+
+            Assert.AreEqual(new BigInteger(4), sum);
+        }
+
+        [Test]
+        public void SumOfFirstFiveFibonacciNumbersIsSeven()
+        {
+            var sum = FibonnaciSum.sumFibToN(5);
+
+            Assert.AreEqual(new BigInteger(7), sum);
+        }
+
+        [Test]
+        public void FibonacciListForFirstFiveEvenTermsIsCorrect()
+        {
+            var fibList = FibonnaciSum.evenTermsFibList(10).ToList();
+
+            Assert.AreEqual(new BigInteger(0), fibList[0]);
+            Assert.AreEqual(new BigInteger(2), fibList[1]);
+            Assert.AreEqual(new BigInteger(8), fibList[2]);
+            Assert.AreEqual(new BigInteger(34), fibList[3]);
+            Assert.AreEqual(new BigInteger(144), fibList[4]);
+        }
+    }
+}
---
+++ EulerFibonacci/FibonnaciSum.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module FibonnaciSum
+
+let rec sum total = function
+    | [] -> total
+    | hd :: tl -> sum (hd + total) tl
+
+let rec fibToN = function
+ | 1 -> 0
+ | 2 -> 1
+ | n -> fibToN (n-1) + fibToN (n-2)
+
+let fibs = (0I, 1I) |> Seq.unfold (fun (a, b) -> Some(a, (b, a + b) ) );;
+
+let fibList n = Seq.take n fibs
+
+let fibListUnderFourMillion = Seq.takeWhile (fun x -> x <= 4000000I) fibs
+
+let evenTermsFibList = fibListUnderFourMillion |> Seq.filter (fun x-> x % 2I = 0I)
+
+let rec sumFibToN n = Seq.fold (fun acc elem -> acc + elem) 0I (fibList n)
+
+let sumFibListUnderFourMillion = Seq.fold(fun accumulator element -> accumulator + element) 0I (evenTermsFibList)
+
+let printSeq seq1 = Seq.iter (printf "%A ") seq1; printfn ""
+
+//let rec sumToN n =
+//    if n = 1
+//    then 1
+//    elif n = 2
+//    then 3
+//    else 4
+
+//let rec factorial n = 
+//    if n = 0 
+//    then 1 
+//    else n * factorial (n - 1)
